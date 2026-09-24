6eb02206ca8e7a9314b09428ac6f1a51373f86f0
forge-compliance <gphewett@gmail.com>
2026-09-24 11:56:14 +0100

    Add Stripe link for ceiling access RAMS

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -7,7 +7,7 @@ const paymentLinks = {
   'working-height-ra': 'https://buy.stripe.com/6oUdR89f93rQ7A34aWf3a02',
   'lone-working-ra': 'https://buy.stripe.com/9B64gy1MH2nMdYr6j4f3a03',
   'pump-replacement-rams': 'https://buy.stripe.com/6oU8wO4YTe6u7A39vgf3a04',
-  'ceiling-access-rams': '',
+  'ceiling-access-rams': 'https://buy.stripe.com/fZu4gy62X4vUg6z5f0f3a05',
   'cleaning-chemical-coshh': '',
   'pool-chemical-coshh': '',
   'plant-room-daily-log': '',
