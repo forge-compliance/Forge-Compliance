8797468a6d24af2125243a48f0dacb222f7ae7f2
forge-compliance <gphewett@gmail.com>
2026-09-24 12:23:39 +0100

    Add Stripe link for water temperature monitoring log

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -11,7 +11,7 @@ const paymentLinks = {
   'cleaning-chemical-coshh': 'https://buy.stripe.com/aFa6oGgHB6E24nR8rcf3a06',
   'pool-chemical-coshh': 'https://buy.stripe.com/fZu3cu2QLgeC5rV5f0f3a07',
   'plant-room-daily-log': 'https://buy.stripe.com/8x29ASdvp9Qe1bFePAf3a08',
-  'water-temperature-log': '',
+  'water-temperature-log': 'https://buy.stripe.com/9B68wO4YT4vUcUnbDof3a09',
   'maintenance-asset-register': '',
   'contractor-compliance-register': ''
 };
